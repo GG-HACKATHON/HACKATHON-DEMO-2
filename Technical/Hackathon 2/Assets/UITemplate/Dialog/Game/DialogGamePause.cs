aacd66f569ddcf03ec370fb68d6cfbea1cc84d90
tatanoi <tatanoi96@gmail.com>
2017-05-14 11:11:46 +0700

    3rd commit

---
+++ Technical/Hackathon 2/Assets/UITemplate/Dialog/Game/DialogGamePause.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogGamePause : MonoBehaviour {
+
+	public void onClickResume()
+    { }
+    public void onClickExit()
+    {
+        DialogManager.Instance.ShowMessageBox("Bạn có muốn thoát không?", MESSAGETYPE.YES_NO, () => this.onExit());
+    }
+    void onExit()
+    { }
+}
