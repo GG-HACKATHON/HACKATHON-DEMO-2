c6ca3f714fafe3c08464ac1d12eb643908ca6e30
Khang Dinh Hoang <taigama@users.noreply.github.com>
2017-05-14 15:19:51 +0700

    Swiped!

---
+++ Technical/Hackathon 2/Assets/Scripts/UI/PInput.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PInput : MonoBehaviour
+{
+    public ZZZ zzz;
+
+
+    private float fingerStartTime = 0.0f;
+    private Vector2 fingerStartPos = Vector2.zero;
+
+    private bool isSwipe = false;
+    private float minSwipeDist = 50.0f;
+    private float maxSwipeTime = 0.5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            zzz.Move(ZZZ.ZDIRECT.Up);
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            zzz.Move(ZZZ.ZDIRECT.Down);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            zzz.Move(ZZZ.ZDIRECT.Left);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            zzz.Move(ZZZ.ZDIRECT.Right);
+
+        if (Input.touchCount > 0)
+        {
+
+            foreach (Touch touch in Input.touches)
+            {
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        /* this is a new touch */
+                        isSwipe = true;
+                        fingerStartTime = Time.time;
+                        fingerStartPos = touch.position;
+                        break;
+
+                    case TouchPhase.Canceled:
+                        /* The touch is being canceled */
+                        isSwipe = false;
+                        break;
+
+                    case TouchPhase.Moved:
+                        
+
+                        float gestureTime = Time.time - fingerStartTime;
+                        float gestureDist = (touch.position - fingerStartPos).magnitude;
+
+                        if (isSwipe && gestureTime < maxSwipeTime && gestureDist > minSwipeDist)
+                        {
+                            Vector2 direction = touch.position - fingerStartPos;
+                            Vector2 swipeType = Vector2.zero;
+
+                            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+                            {
+                                // the swipe is horizontal:
+                                swipeType = Vector2.right * Mathf.Sign(direction.x);
+                            }
+                            else
+                            {
+                                // the swipe is vertical:
+                                swipeType = Vector2.up * Mathf.Sign(direction.y);
+                            }
+
+                            if (swipeType.x != 0.0f)
+                            {
+                                if (swipeType.x > 0.0f)
+                                {
+                                    // MOVE RIGHT
+                                    zzz.Move(ZZZ.ZDIRECT.Right);
+
+                                }
+                                else
+                                {
+                                    // MOVE LEFT
+                                    zzz.Move(ZZZ.ZDIRECT.Left);
+                                }
+                            }
+
+                            if (swipeType.y != 0.0f)
+                            {
+                                if (swipeType.y > 0.0f)
+                                {
+                                    // MOVE UP
+                                    zzz.Move(ZZZ.ZDIRECT.Up);
+                                }
+                                else
+                                {
+                                    // MOVE DOWN
+                                    zzz.Move(ZZZ.ZDIRECT.Down);
+                                }
+                            }
+
+                        }
+
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Technical/Hackathon 2/Assets/Scripts/UI/ZZZ.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZZZ : MonoBehaviour {
+
+    public enum ZDIRECT
+    {
+        None,
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public Vector3 mover;
+    public float speed;
+
+
+	void Update ()
+    {
+        transform.position += mover * Time.deltaTime * speed;
+
+        
+
+    }
+
+
+    public void Move(ZDIRECT dir)
+    {
+        switch (dir)
+        {
+            case ZDIRECT.Up:
+                {
+                    mover = Vector2.up;
+                }
+                break;
+            case ZDIRECT.Down:
+                mover = Vector2.down;
+                break;
+            case ZDIRECT.Left:
+                mover = Vector2.left;
+                break;
+            case ZDIRECT.Right:
+                mover = Vector2.right;
+                break;
+            default:
+                break;
+        }
+    }
+
+}
